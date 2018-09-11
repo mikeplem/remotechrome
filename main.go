91308665043e08f69da4d91f095a10b34a65c3ec
Mike Plemmons <mikeplem@users.noreply.github.com>
2018-09-10 21:13:49 -0400

    attempts at a reset endpoint - post needs fixed

--- main.go
+++ main.go
@@ -1,42 +1,75 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/raff/godet"
 )
 
 func main() {
 
-	currentURL := flag.Bool("current", false, "print current URL open")
-	openURL := flag.String("open", "", "URL to open in browser")
+	// currentURL := flag.Bool("current", false, "print current URL open")
+	// openURL := flag.String("open", "", "URL to open in browser")
 
-	flag.Parse()
+	// flag.Parse()
 
-	// connect to Chrome instance
+	// user requested the current open URL
+	// if *currentURL {
+	// 	tabs, _ := remoteConn.TabList("")
+	// 	for _, value := range tabs {
+	// 		if value.Type != "background_page" {
+	// 			fmt.Printf("%s,%s\n", value.Title, value.URL)
+	// 		}
+	// 	}
+	// }
+
+	// user wants to open a new URL
+	// if *openURL != "" {
+	// 	_, _ = remoteConn.Navigate(*openURL)
+	// }
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", printCurrentURL).Methods("GET")
+	router.HandleFunc("/current", printCurrentURL).Methods("GET")
+	router.HandleFunc("/open", openURLInBrowser).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func printCurrentURL(w http.ResponseWriter, r *http.Request) {
+
 	remote, err := godet.Connect("localhost:9222", false)
 	if err != nil {
-		fmt.Println("cannot connect to Chrome instance:", err)
+		fmt.Fprintln(w, "cannot connect to Chrome instance:", err)
+		return
+	}
+
+	// disconnect when done
+	defer remote.Close()
+	tabs, _ := remote.TabList("")
+	for _, value := range tabs {
+		if value.Type != "background_page" {
+			fmt.Fprintln(w, value.Title, value.URL)
+		}
+	}
+}
+
+func openURLInBrowser(w http.ResponseWriter, r *http.Request) {
+
+	remote, err := godet.Connect("localhost:9222", false)
+	if err != nil {
+		fmt.Fprintln(w, "cannot connect to Chrome instance:", err)
 		return
 	}
 
 	// disconnect when done
 	defer remote.Close()
 
-	// user requested the current open URL
-	if *currentURL {
-		tabs, _ := remote.TabList("")
-		for _, value := range tabs {
-			if value.Type != "background_page" {
-				fmt.Printf("%s,%s\n", value.Title, value.URL)
-			}
-		}
-	}
-
-	// user wants to open a new URL
-	if *openURL != "" {
-		_, _ = remote.Navigate(*openURL)
-	}
-
+	vars := mux.Vars(r)
+	openURL := vars["url"]
+	fmt.Println(openURL)
+	_, _ = remote.Navigate(openURL)
 }
